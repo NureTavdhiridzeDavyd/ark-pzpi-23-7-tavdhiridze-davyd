8710b8e3ae741164d85bbae7c160041aeeb339fb
Tavdhiridze Davyd <davyd.tavdhiridze@nure.ua>
2026-04-16 18:32:27 +0200

    im tired

--- ark-pzpi-23-7-tavdhiridze-davyd/hivetrack/src/errors/BusinessError.js
+++
@@ -1,9 +0,0 @@
-class BusinessError extends Error {
-  constructor(message, statusCode = 400) {
-    super(message);
-    this.name = 'BusinessError';
-    this.statusCode = statusCode;
-  }
-}
-
-module.exports = { BusinessError };
